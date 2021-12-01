bb8b8c722281cdc4473d40f867d4ecc9530b0d9e
TheHugo <the.hugo.lom@gmail.com>
2021-12-01 11:21:54 +0100

    configure default answers

--- src/comp_features.rs
+++ src/comp_features.rs
@@ -3,7 +3,7 @@ pub fn fetch_default(day: usize, part: usize) -> String {
     if 2 < part || part == 0 {
         format!("Invalid part {}", part)
     } else {
-        let p = format!("data/default/day{}.txt", day);
+        let p = format!("default/day{}.txt", day);
 
         match std::fs::read_to_string(p) {
             Ok(p) => match p.splitn(2, "\r\n").into_iter().skip(part - 1).next() {
