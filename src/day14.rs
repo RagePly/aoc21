6f948217dc6b9e4d268a4308cc5f346b715e11dd
TheHugo <the.hugo.lom@gmail.com>
2021-12-15 00:56:41 +0100

    day 14

--- src/day14.rs
+++ src/day14.rs
@@ -17,15 +17,52 @@ pub fn part2(_source: String) -> String {
 use std::collections::HashMap;
 
 #[cfg(feature = "day14")]
-fn recurse(left: char, right: char, depth: usize, max_depth: usize, mapping: &HashMap<[char; 2], char>, scores: &mut HashMap<char, usize>) {
-    if depth > max_depth {
-        return
+type Pair = [char; 2];
+
+#[cfg(feature = "day14")]
+type Score = HashMap<char, usize>;
+
+#[cfg(feature = "day14")]
+type ScoreChache = HashMap<Pair, Score>;
+
+#[cfg(feature = "day14")]
+type LevelCache = Vec<ScoreChache>;
+
+#[cfg(feature = "day14")]
+type Insertions = HashMap<Pair, char>;
+
+#[cfg(feature = "day14")]
+fn scoring_recursive(
+    pair: Pair, 
+    insertions: &Insertions,
+    level_cache: &mut LevelCache,
+    depth: usize,
+) {
+    let mut scoring = Score::new();
+    let new = insertions[&pair];
+    scoring.insert(new, 1);
+
+    if depth != 0 {
+        [[pair[0], new], [new, pair[1]]]
+        .iter()
+        .for_each(|new_pair| {
+            if level_cache.len() <= depth - 1 || !level_cache[depth - 1].contains_key(new_pair) {
+                // is guaranteed to insert new_pair into 
+                scoring_recursive(*new_pair, insertions, level_cache, depth - 1);
+            }
+
+            level_cache[depth - 1][new_pair]
+            .iter()
+            .for_each(|(c, s)| *scoring.entry(*c).or_insert(0) += s);
+        });
+    }
+
+    if level_cache.len() == depth { 
+        let mut score_cache = ScoreChache::new();
+        score_cache.insert(pair, scoring);
+        level_cache.push(score_cache);
     } else {
-        // get new
-        let new = mapping[&[left, right]];
-        *scores.entry(new).or_insert(0) += 1;
-        recurse(left, new, depth + 1, max_depth, mapping, scores);
-        recurse(new, right, depth + 1, max_depth, mapping, scores);
+        level_cache[depth].insert(pair, scoring);
     }
 }
 
@@ -33,35 +70,100 @@ fn recurse(left: char, right: char, depth: usize, max_depth: usize, mapping: &Ha
 pub fn part1(source: String) -> usize {
     let mut parts = source.split("\r\n\r\n");
     let orig_state: Vec<_> = parts.next().unwrap().chars().collect();
-    let mapping: HashMap<[char; 2], char> = parts.next().unwrap()
+    let mapping: Insertions = parts
+    .next()
+    .unwrap()
     .split("\r\n")
     .map(|line| {
         let mut terms = line.split(" -> ");
         let mut temp = terms.next().unwrap().chars();
-        let from = [temp.next().unwrap(), temp.next().unwrap()];
-        let to = terms.next().unwrap().chars().next().unwrap();
+        let from: Pair = [temp.next().unwrap(), temp.next().unwrap()];
+        let to: char = terms.next().unwrap().chars().next().unwrap();
         (from, to)
     })
     .collect();
 
-    let mut scoring: HashMap<char, usize> = HashMap::new();
-    orig_state
-    .iter()
-    .for_each(|&c| *scoring.entry(c).or_insert(0) += 1);
-
+    // apply recursive function
+    let mut level_cache = LevelCache::new(); 
     orig_state
     .as_slice()
     .windows(2)
     .for_each(|v|
-        recurse(v[0], v[1], 1, 10, &mapping, &mut scoring)
+        scoring_recursive([v[0], v[1]], &mapping, &mut level_cache, 9)
     );
 
+    // add accumulated scores
+    let mut scoring = Score::new();
+    level_cache[9]
+    .iter()
+    .for_each(|(_, scores)| 
+        scores
+        .iter()
+        .for_each(|(c, s)| 
+            *scoring.entry(*c).or_insert(0) += s
+        )
+    );
+
+    // add original letters to score
+    orig_state
+    .iter()
+    .for_each(|&c| 
+        *scoring.entry(c).or_insert(0) += 1
+    );
+
+    // collect scores and sort
     let mut scores: Vec<_> = scoring.values().map(|&v| v).collect();
     scores.sort_unstable();
     scores[scores.len() - 1] - scores[0]
 }
 
 #[cfg(feature = "day14")]
-pub fn part2(source: String) -> String {
-    String::from("not implemented")
+pub fn part2(source: String) -> usize {
+    let mut parts = source.split("\r\n\r\n");
+    let orig_state: Vec<_> = parts.next().unwrap().chars().collect();
+    let mapping: Insertions = parts
+    .next()
+    .unwrap()
+    .split("\r\n")
+    .map(|line| {
+        let mut terms = line.split(" -> ");
+        let mut temp = terms.next().unwrap().chars();
+        let from: Pair = [temp.next().unwrap(), temp.next().unwrap()];
+        let to: char = terms.next().unwrap().chars().next().unwrap();
+        (from, to)
+    })
+    .collect();
+
+    // apply recursive function
+    let mut level_cache = LevelCache::new(); 
+    orig_state
+    .as_slice()
+    .windows(2)
+    .for_each(|v|
+        scoring_recursive([v[0], v[1]], &mapping, &mut level_cache, 39)
+    );
+
+    // add accumulated scores
+    let mut scoring = Score::new();
+    level_cache[39]
+    .iter()
+    .for_each(|(_, scores)| 
+        scores
+        .iter()
+        .for_each(|(c, s)| 
+            *scoring.entry(*c).or_insert(0) += s
+        )
+    );
+
+    // add original letters to score
+    orig_state
+    .iter()
+    .for_each(|&c| 
+        *scoring.entry(c).or_insert(0) += 1
+    );
+
+    // collect scores and sort
+    let mut scores: Vec<_> = scoring.values().map(|&v| v).collect();
+    scores.sort_unstable();
+    scores[scores.len() - 1] - scores[0]
 }
